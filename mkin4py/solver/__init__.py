b51577af202d00dbbefdf4183857c926c8d67048
Gabriel S. Gusmão <gusmaogabriels@gmail.com>
2015-10-27 21:17:00 -0200

    Create __init__.py

---
+++ mkin4py/solver/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, absolute_import, print_function
+
+from .. import __author__, __version__, np, linalg, time, dc
+from . import solve, coverage_fix, derivatives, linsolver
+
+__all__ = ['__author__','__version__','coverage_fix','derivatives','linsolver','solve','np','dc','time']
