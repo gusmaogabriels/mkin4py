53022771cba02806c24e86de49dc53d50293fd14
Gabriel S. Gusmão <gusmaogabriels@gmail.com>
2018-11-11 12:39:04 -0500

    Update __init__.py
    Reference fix.

--- mkin4py/solver/__init__.py
+++ mkin4py/solver/__init__.py
@@ -3,6 +3,6 @@
 from __future__ import division, absolute_import, print_function
 
 from .. import __author__, __version__, np, linalg, time, dc
-from . import solve, coverage_fix, derivatives, linsolver
+from . import solve, coverage, derivatives, linsolver
 
-__all__ = ['__author__','__version__','coverage_fix','derivatives','linsolver','solve','np','dc','time']
+__all__ = ['__author__','__version__','coverage','derivatives','linsolver','solve','np','dc','time']
